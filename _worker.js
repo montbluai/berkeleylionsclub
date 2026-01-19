a5a993994eb98ab51ccf413133c2341a403679e4
Ami Saunders <amisaunders@Amis-MacBook-Pro.local>
2026-01-18 22:25:30 -0800

    Add worker to serve static assets

---
+++ _worker.js
@@ -0,0 +1,15 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red242\green242\blue242;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0\c90196;\cssrgb\c96078\c96078\c96078;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24\fsmilli12220 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 export default \{\
+  async fetch(request, env) \{\
+    return env.ASSETS.fetch(request);\
+  \}\
+\};\
+}
